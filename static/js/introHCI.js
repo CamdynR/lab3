fbfbb6c6a2df3f59dd78a3e5203866123da73e3c
CamdynR <rasque.camdyn@gmail.com>
2019-01-24 10:47:35 -0800

    First commit

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -3,6 +3,7 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+	projectClick();
 })
 
 /*
@@ -15,4 +16,18 @@ function initializePage() {
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e) {
+	$("#testjs").click(function(e) {
+		var containingProject = "#project1"; //$(this).closest(".project");
+		var description = $(containingProject).find(".project-description");
+		$("#testjs").text("This button is mine");
+		$("#testjs").addClass("active");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       $(description).html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
+	});
 }
